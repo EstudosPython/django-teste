ef2edddf23c020be8af108585c6fdf299ffea4ee
Micro do Guto <gustavoguesser@gmail.com>
2021-07-02 23:39:55 -0300

    corrigido Heroku

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # from django.shortcuts import render
 
 
-# Create your views here.
+# Create your views here (codigo aqui).
 
 def home(request):
     return HttpResponse('Ola Django')
