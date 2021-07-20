29f6baae6e8201797536ad5cca74fbaf14d42d7e
Micro do Guto <gustavoguesser@gmail.com>
2021-07-20 14:38:32 -0300

    Adicionado videos promocionais com vimeo close #54

---
+++ pypro/aperitivos/tests.py
@@ -0,0 +1,22 @@
+import pytest
+from django.urls import reverse
+
+from pypro.django_assertions import assert_contains
+
+
+@pytest.fixture
+def resp(client):
+    return client.get(reverse('aperitivos:video', args=('inicial',)))
+
+
+def test_status_code(resp):
+    assert resp.status_code == 200
+
+
+def test_titulo_video(resp):
+    assert_contains(resp, 'Video aperitivo: inicial</h1>')
+
+
+def test_conteudo_video(resp):
+    assert_contains(resp,
+                    '<iframe src="https://player.vimeo.com/video/577177067?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"')
