19988555d77400bffd3ae3614aeda52b3c5803d8
Micro do Guto <gustavoguesser@gmail.com>
2021-07-21 16:04:59 -0300

    Corrigido

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 
 
+def indice(request):
+    return render(request, 'aperitivos/indice.html')
+
+
 def video(request, slug):
     videos = {
-        'motivacao':{'titulo': 'Video aperitivo: inicial', 'vimeo_id': 577177067},
-        'instalacao-windows':{'titulo': 'instalação Windows', 'vimeo_id': 251497668},
+        'motivacao': {'titulo': 'Video aperitivo: inicial', 'vimeo_id': 577177067},
+        'instalacao-windows': {'titulo': 'instalação Windows', 'vimeo_id': 251497668},
     }
     video = videos[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
