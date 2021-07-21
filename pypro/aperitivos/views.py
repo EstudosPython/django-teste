6f5272154c97165dfa2a446042bd9cc1633c9654
Micro do Guto <gustavoguesser@gmail.com>
2021-07-21 09:58:18 -0300

    falha nos testes

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    return render(request, 'aperitivos/video.html')
+    videos = {
+        'motivacao':{'titulo': 'Video aperitivo: inicial', 'vimeo_id': 577177067},
+        'instalacao-windows':{'titulo': 'instalação Windows', 'vimeo_id': 251497668},
+    }
+    video = videos[slug]
+    return render(request, 'aperitivos/video.html', context={'video': video})
