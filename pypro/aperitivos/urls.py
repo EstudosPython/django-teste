43c1060115cfe698446723f92fa0d6ffe91e7468
Micro do Guto <gustavoguesser@gmail.com>
2021-07-22 09:37:48 -0300

    Finalizado a parte de template close #56

--- pypro/aperitivos/urls.py
+++ pypro/aperitivos/urls.py
@@ -5,5 +5,5 @@ from pypro.aperitivos.views import video, indice
 app_name = 'aperitivos'
 urlpatterns = [
     path('<slug:slug>', video, name='video'),
-    path('', indice, name='indices'),
+    path('', indice, name='indice'),
 ]
--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render
+from django.urls import reverse
+
+
+class Video:
+    def __init__(self, slug, titulo, vimeo_id):
+        self.slug = slug
+        self.titulo = titulo
+        self.vimeo_id = vimeo_id
+
+    def get_absolute_url(self):
+        return reverse('aperitivos:video', args=(self.slug,))
+
+
+videos = [
+    Video('motivacao', 'Video aperitivo: inicial', 577177067),
+    Video('instalacao-windows', 'instalação Windows', 251497668),
+]
+
+videos_dct = {v.slug: v for v in videos}
 
 
 def indice(request):
-    return render(request, 'aperitivos/indice.html')
+    return render(request, 'aperitivos/indice.html', context={'videos': videos})
 
 
 def video(request, slug):
-    videos = {
-        'motivacao': {'titulo': 'Video aperitivo: inicial', 'vimeo_id': 577177067},
-        'instalacao-windows': {'titulo': 'instalação Windows', 'vimeo_id': 251497668},
-    }
-    video = videos[slug]
+    video = videos_dct[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
