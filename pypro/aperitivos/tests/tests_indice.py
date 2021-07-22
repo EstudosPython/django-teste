2783e6d1a1ebb235eec0d44ae17bc6712c4700b9
Micro do Guto <gustavoguesser@gmail.com>
2021-07-22 09:37:48 -0300

    Finalizado a parte de template close #56

--- pypro/aperitivos/tests/tests_indice.py
+++ pypro/aperitivos/tests/tests_indice.py
@@ -7,17 +7,31 @@ from pypro.django_assertions import assert_contains
 
 @pytest.fixture
 def resp(client):
-    return client.get(reverse('aperitivos:indices'))
+    return client.get(reverse('aperitivos:indice'))
 
 
 def test_status_code(resp):
     assert resp.status_code == 200
 
 
-# def test_titulo_video(resp):
-#     assert_contains(resp, 'Video aperitivo: inicial')
-#
-#
-# def test_conteudo_video(resp):
-#     assert_contains(resp,
-#                     '<iframe src="https://player.vimeo.com/video/577177067')
+@pytest.mark.parametrize(
+    'titulo',
+    [
+        'Video aperitivo: inicial',
+        'instalação Windows'
+    ]
+)
+def test_titulo_video(resp, titulo):
+    assert_contains(resp, titulo)
+
+
+@pytest.mark.parametrize(
+    'slug',
+    [
+        'motivacao',
+        'instalacao-windows'
+    ]
+)
+def test_link_video(resp, slug):
+    video_link = reverse('aperitivos:video', args=(slug,))
+    assert_contains(resp, f'href="{video_link}"')
--- pypro/base/tests/test_home.py
+++ pypro/base/tests/test_home.py
@@ -21,3 +21,8 @@ def test_title(resp):
 
 def test_home_link(resp):
     assert_contains(resp, f'href="{reverse("base:home")}">Python Pro</a>')
+
+
+def test_email_link(resp):
+    contato_email = 'gustavoguesser@gmail.com'
+    assert_contains(resp, f'href="mailto:{contato_email}')
