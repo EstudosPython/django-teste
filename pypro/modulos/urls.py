975bca0c950586e5bfb4087775d27ebc9397cac0
Micro do Guto <gustavoguesser@gmail.com>
2021-08-06 20:34:16 -0300

    Finalizado os modulos n pra n

--- pypro/modulos/urls.py
+++ pypro/modulos/urls.py
@@ -6,5 +6,6 @@ app_name = 'modulos'
 urlpatterns = [
     path('<slug:slug>', views.detalhe, name='detalhe'),
     path('aulas/<slug:slug>', views.aula, name='aula'),
+    path('', views.indice, name='indice'),
 
 ]
